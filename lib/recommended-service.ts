3784c093d763f6548c5bc525016aa65f265e36ea
Sudhanshu Bharti <120251980+Sudhanshu-Bharti@users.noreply.github.com>
2024-02-27 00:55:22 +0530

    Update recommended-service.ts

--- lib/recommended-service.ts
+++ lib/recommended-service.ts
@@ -2,12 +2,35 @@ import { db } from "./db";
 import {getself} from './auth-service'
 
 export const getRecommended = async ()=> {
+    let userId
 
-    const users = await db.user.findMany({
-        orderBy: {
-            CreatedAt: 'desc'
-        }
-    })
+    try {
+        const selfId = await getself()
+         userId = selfId 
+    } catch {
+         userId = null
+    }
+    
+    let users = []
+
+    if (userId) {
+        users = await db.user.findMany({
+            where:{
+                NOT:{
+                    id: userId.id 
+                }
+            },
+            orderBy: {
+                CreatedAt: 'desc'
+            }
+        })
+    } else {
+        users = await db.user.findMany({
+            orderBy: {
+                CreatedAt: 'desc'
+            }
+        })   
+    }
 
     return users
 }
